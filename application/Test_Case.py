8c98d4014e2034577be12df7ff4b6c384357ec18
zaineb-damak <77733791+zaineb-damak@users.noreply.github.com>
2023-07-20 10:57:05 +0100

    deleted and added commnets

--- application/Test_Case.py
+++ application/Test_Case.py
@@ -1,7 +1,6 @@
 import pandas as pd
 import json
 from openpyxl import load_workbook
-
 from Connected_Devices import *
 
 #returns global station number corresponding to the given device id
@@ -46,70 +45,6 @@ def newExcel(wb, row, column,data,new_file):
     ws = wb.active
     ws.cell(row=row, column=column+1).value = data
     wb.save(filename = new_file +'.xlsx')
-
-
-
-# def updateTestCase(file_path,IDList,new_file):
-#     #create a new dataframe
-#     print('start')
-#     df = pd.read_excel(file_path, header=56)
-#     #empty list linking the old phone number to the new phone number
-#     corresponding_phone = []
-#     #initiating variables which will be later used in newExcel
-#     excel_row = []
-#     columns = df.columns
-#     column = 0
-#     #get the number of the column ['Action Field 3] in which changes will be made
-#     id_list = idList(IDList)
-#     print(id_list)
-#     for k in range (0,len(columns)):
-#         if columns[k] == 'Action Field 3':
-#             column = k
-#             break
-#     #changing column action field into string
-#     df['Action Field 1 ( Tag Name/Request Name/ Picture Name)'] = df['Action Field 1 ( Tag Name/Request Name/ Picture Name)'].astype(str)
-#     #iterating through each row 
-#     for index,row in df.iterrows():
-#         #check if there is a phone being used
-#         if('Phone' in row['Action Field 1 ( Tag Name/Request Name/ Picture Name)']):
-#             #each iteration add to the list the index of the row in which we made changes 
-#             excel_row.append(index+58)
-#             #if Action Field 3 is empty we don't execute the rest of the instructions
-#             if pd.isnull(row['Action Field 3'])==True:
-#                 continue
-#             #case 1: corresponding_phone is empty
-#             if (len(corresponding_phone) == 0 ):
-#                 #add element to list [old gsnumber, new gsnumber]
-#                 corresponding_phone.append([row['Action Field 3'],int(getGSNumber(id_list[0]))])
-#                 row['Action Field 3'] = int(getGSNumber(id_list[0]))
-#                 df.loc[index,'Action Field 3']=row['Action Field 3'] 
-#                 id_list.pop(0)
-#             else:
-#                 found_matching_pair = False
-#                 x = 0
-#                 for i in range (0,len(corresponding_phone)):
-#                     x =  int(corresponding_phone[i][0])
-                    
-#                     if(row['Action Field 3'] == int(corresponding_phone[i][0])):
-#                         found_matching_pair = True
-#                         x = i
-#                         break
-#                 #case 2: phone number exists in corresponding_phone
-#                 if (found_matching_pair == True):
-#                     #replace the old value with the new value in dataframe
-#                     row['Action Field 3'] = int(corresponding_phone[x][1])
-#                     df.loc[index,'Action Field 3'] = row['Action Field 3'] 
-#                 #case 3: phone number doesn't exist in corresponding_phone
-#                 else:
-#                     corresponding_phone.append([row['Action Field 3'],int(getGSNumber(id_list[0]))])
-#                     row['Action Field 3'] = getGSNumber(id_list[0])
-#                     df.loc[index,'Action Field 3']  = row['Action Field 3'] 
-#                     id_list.pop(0)
-        
-#     print(df['Action Field 3'])
-#     wb = load_workbook(file_path)
-#     for j in range (0,len(excel_row)):
-#         newExcel(wb, excel_row[j] ,column,df.loc[excel_row[j]-58,'Action Field 3'], new_file)
 
 
 def updateTestCase(file_path,IDList,new_file):
@@ -167,6 +102,7 @@ def updateTestCase(file_path,IDList,new_file):
                     df.loc[index,'Action Field 3']  = row['Action Field 3'] 
                     id_list.pop(0)
         
+        #changing the names of the devices
         names = deviceNames()
 
         if str(row['Action Field 3']).replace('"','') in  names:
@@ -189,10 +125,13 @@ def updateTestCase(file_path,IDList,new_file):
 
         print(row['Action Field 3'])
    
-   
+    #saving the changes in a new excel file
     wb = load_workbook(file_path)
     for j in range (0,len(excel_row)):
         newExcel(wb, excel_row[j] ,column,df.loc[excel_row[j]-58,'Action Field 3'], new_file)
+
+
+
 
 #returns list of names of test cases in the execution plan
 def getTestCases(execution_plan_file_path):
@@ -202,7 +141,7 @@ def getTestCases(execution_plan_file_path):
         file_name.append(row['Test Name'] + '.xlsx') 
     return(file_name)
 
-
+#updates the test cases present in the execution plan
 def executionPlan(execution_plan_file_path, idList):
     testcases = []
     testcases = getTestCases(execution_plan_file_path)
