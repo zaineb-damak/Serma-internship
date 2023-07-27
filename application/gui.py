6f9f85d2cb6a4f04832adaa3dc61668c25531ddb
zaineb-damak <77733791+zaineb-damak@users.noreply.github.com>
2023-07-27 12:28:28 +0100

    cleaned code

--- application/gui.py
+++ application/gui.py
@@ -17,11 +17,12 @@ ctk.set_default_color_theme("green")
 # Dimensions of the window
 appWidth, appHeight = 800, 900
 
-
+# allows user to select a file
 def browseFiles():
     filename = filedialog.askopenfilename(initialdir = "/",title = "Select a File",filetypes = (("Excel files","*.xlsx*"),))
     return filename
 
+# asks a user to choose where to save the file
 def choose_directory():
     global directory
     directory = filedialog.askdirectory()
@@ -31,13 +32,13 @@ def choose_directory():
     changeButton2 = ctk.CTkButton(master=root,text="Select execution plan",command=executionPlanArg)
     changeButton2.grid(row=6, column=0,columnspan=2, padx=20,pady=20, sticky="ew")
 
-def choose_directory2():
-    global directory2
-    directory2 = filedialog.askdirectory()
-    if directory2:
-        executionPlan(file,directory2,"List of devices attached R59RA00NL7D device LMG900EMf7a2d5d5 device R58M36NV1GD device R58N91KCNYY device 215cf1f7 device",directory)    
-        messageUpdated2()
 
+
+
+
+#####  test case #####
+
+# displays message after the changes are made
 def messageUpdated1():
      global pop
      pop = Toplevel(root)
@@ -50,42 +51,7 @@ def messageUpdated1():
      ok = ctk.CTkButton(pop_frame, text="see changes", command=getNewAndOld1)
      ok.grid(row=2,column=0)
 
-def messageUpdated2():
-     global pop
-     pop = Toplevel(root)
-     pop.title("message")
-     pop.geometry("250x150")
-     pop_frame = Frame(pop)
-     pop_frame.pack(pady=5)
-     pop_label = ctk.CTkLabel(master=pop_frame,text="file updated successfully")
-     pop_label.grid(row=1,column=0)
-     ok = ctk.CTkButton(pop_frame, text="see changes", command=getNewAndOld2)
-     ok.grid(row=20,column=0)
-
-def messagePath():
-    pop = Toplevel(root)
-    pop.title("choose path")
-    pop.geometry("750x150")
-    pop_frame = Frame(pop)
-    pop_frame.pack(pady=5)
-    pop_label = ctk.CTkLabel(master=pop_frame,text="select the folder in which the test case files are saved")
-    pop_label.grid(row=1,column=0)
-    ok = ctk.CTkButton(pop_frame, text="browse", command=choose_directory2)
-    ok.grid(row=2,column=0)
-    
-
-def updateTestCaseArg():
-    user_input = simpledialog.askstring("Input", "Enter the name of the updated file")
-    file = browseFiles()
-    updateTestCase(file,"List of devices attached R59RA00NL7D device LMG900EMf7a2d5d5 device R58M36NV1GD device R58N91KCNYY device 215cf1f7 device",user_input,directory)
-    messageUpdated1()
-
-def executionPlanArg():
-    global file
-    file = browseFiles()
-    messagePath()
-    
-#test case
+# displays the changes made
 def getNewAndOld1():
     global pop
     pop = Toplevel(root)
@@ -102,6 +68,53 @@ def getNewAndOld1():
     ok = ctk.CTkButton(pop_frame, text="ok", command=pop.destroy)
     ok.grid(row=6,column=0)
      
+def updateTestCaseArg():
+    user_input = simpledialog.askstring("Input", "Enter the name of the updated file")
+    file = browseFiles()
+    updateTestCase(file,"List of devices attached R59RA00NL7D device LMG900EMf7a2d5d5 device R58M36NV1GD device R58N91KCNYY device 215cf1f7 device",user_input,directory)
+    messageUpdated1()
+
+
+#####   execution plan   #####
+
+# asks user where the test case files are saved
+def messagePath():
+    global pop
+    pop = Toplevel(root)
+    pop.title("choose path")
+    pop.geometry("750x150")
+    pop_frame = Frame(pop)
+    pop_frame.pack(pady=5)
+    pop_label = ctk.CTkLabel(master=pop_frame,text="select the folder in which the test case files are saved")
+    pop_label.grid(row=1,column=0)
+    ok = ctk.CTkButton(pop_frame, text="browse", command=choose_directory2)
+    ok.grid(row=2,column=0)
+    
+
+# allows user to chose the path in which test case files are saved
+def choose_directory2():
+    global directory2
+    pop.destroy()
+    directory2 = filedialog.askdirectory()
+    if directory2:
+        executionPlan(file,directory2,"List of devices attached R59RA00NL7D device LMG900EMf7a2d5d5 device R58M36NV1GD device R58N91KCNYY device 215cf1f7 device",directory)    
+        messageUpdated2()
+
+# displays message after the changes are made
+def messageUpdated2():
+     global pop
+     pop = Toplevel(root)
+     pop.title("message")
+     pop.geometry("250x150")
+     pop_frame = Frame(pop)
+     pop_frame.pack(pady=5)
+     pop_label = ctk.CTkLabel(master=pop_frame,text="file updated successfully")
+     pop_label.grid(row=1,column=0)
+     ok = ctk.CTkButton(pop_frame, text="see changes", command=getNewAndOld2)
+     ok.grid(row=20,column=0)
+
+
+# displays the changes made
 def getNewAndOld2():
     global pop
     pop = Toplevel(root)
@@ -135,19 +148,16 @@ def getNewAndOld2():
 
     # Add "OK" button to close the pop-up
     ok = ctk.CTkButton(master=pop_frame, text="OK", command=pop.destroy)
-    ok.grid(row=len(changes), column=0, padx=5, pady=5)
+    ok.grid(row=len(changes), column=0, padx=5, pady=5)   
+
+def executionPlanArg():
+    global file
+    file = browseFiles()
+    messagePath()
+    
 
 
-
-
-
-
-
-
-
-
-
-
+##### main window #####
 
 
 root = ctk.CTk()
